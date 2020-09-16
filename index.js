b3184a2b281abe67470b7ed7aebabb5e93913adb
Nick Warner <nickwarner@gmail.com>
2020-09-16 12:04:19 -0400

    Add searching for films by string, listing all films at once, and simplified provider info

--- index.js
+++ index.js
@@ -16,13 +16,42 @@ function readData() {
   }
 }
 
+function renderFilmString(number, film) {
+  console.log(
+    `#${number}: "${film.title}" - ${film.year} - ${film.director} - ${film.length} minutes`,
+  )
+}
+
+function listAll() {
+  const data = readData()
+  data.films.forEach((film, index) => {
+    renderFilmString(index + 1, film)
+  })
+}
+
+async function findFilm({ query }) {
+  query = query.toString().toLowerCase()
+  console.log(`Searching for: "${query}"`)
+  console.log('')
+
+  const data = readData()
+  data.films.forEach((film, index) => {
+    if (
+      film.title.toLowerCase().includes(query) ||
+      film.year.toString().includes(query) ||
+      film.director.toLowerCase().includes(query) ||
+      film.country.toLowerCase().includes(query)
+    ) {
+      renderFilmString(index + 1, film)
+    }
+  })
+}
+
 async function getFilm({ number }) {
   const data = readData()
   const film = data.films[number - 1]
   if (film) {
-    console.log(
-      `#${number}: "${film.title}" - ${film.year} - ${film.director} - ${film.length} minutes`,
-    )
+    renderFilmString(number, film)
     console.log('')
     const justwatch = new JustWatch()
     const providers = await justwatch.getProviders()
@@ -49,16 +78,37 @@ async function getFilm({ number }) {
         return priceA > priceB ? 1 : -1
       })
 
+      let finalOffers = []
+      if (verbose) {
+        finalOffers = offers
+      } else {
+        // Remove duplicates
+        let uniqueOffers = []
+        offers = offers.forEach((offer) => {
+          const include = !uniqueOffers.some((unique) => {
+            return offer.provider_id === unique.provider_id
+          })
+          if (include) {
+            uniqueOffers.push(offer)
+          }
+        })
+
+        // Limit results
+        finalOffers = uniqueOffers.slice(0, 4)
+      }
+
       console.log('Available at:')
-      offers.forEach((offer) => {
+      finalOffers.forEach((offer) => {
         if (offer.provider) {
           let price = ''
           if (offer.retail_price) {
             price = ` - $${offer.retail_price}`
           }
-          console.log(
-            `- ${offer.provider.clear_name} - ${offer.presentation_type}${price}`,
-          )
+          let format = ''
+          if (verbose) {
+            format = ` - ${offer.presentation_type}`
+          }
+          console.log(`- ${offer.provider.clear_name}${format}${price}`)
         }
       })
     }
@@ -67,15 +117,35 @@ async function getFilm({ number }) {
 }
 
 const help = `
-1000films - where to watch the 1000 greatest films as ranked by theyshootpictures.com, 2020 version
+1000films: where to watch the 1000 greatest films as ranked by theyshootpictures.com, 2020 version
 
-How to use:
-$1000films [number]
+Show film info and where to watch by rank:
+$ 1000films [number]
+
+Show more verbose results:
+$ 1000films [number] -v
+
+Search for films:
+$ 1000films [search string]
+
+List all films:
+$ 1000films -l
 `
 
 const args = process.argv.slice(2)
-if (args[0]) {
-  getFilm({ number: args[0] })
+const query = args[0]
+const verbose = args.includes('-v')
+if (query) {
+  if (query === '-l') {
+    listAll()
+  } else {
+    const number = parseInt(query)
+    if (number && number <= 1000) {
+      getFilm({ number })
+    } else {
+      findFilm({ query })
+    }
+  }
 } else {
   console.log(help)
 }
--- readme.md
+++ readme.md
@@ -4,9 +4,20 @@ a command line program to find where to watch the 1000 greatest films as ranked 
 
 ## How to use
 
+Install:
 `$ npm i -g 1000films`
 
+Show film info and where to watch by rank:
 `$ 1000films [number]`
+
+Show more verbose where to watch results:
+`$ 1000films [number] -v`
+
+Search for films:
+`$ 1000films [search string]`
+
+List all films:
+`$ 1000films -l`
 
 ## Credits
 
