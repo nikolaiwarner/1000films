fd16492f85c0051fc1c40e27a4dc02bbf7f1ab3c
Nick Warner <nickwarner@gmail.com>
2024-02-21 14:58:43 -0500

    remove broken justwatch api code; simplify by showing a url to streaming info instead

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 const fs = require('fs')
 const path = require('path')
-const JustWatch = require('justwatch-api')
 
 function readData() {
   try {
@@ -53,66 +52,9 @@ async function getFilm({ number }) {
   if (film) {
     renderFilmString(number, film)
     console.log('')
-    const justwatch = new JustWatch()
-    const providers = await justwatch.getProviders()
-    const searchResult = await justwatch.search({
-      content_types: ['movie'],
-      query: film.title,
-      release_year_from: film.year,
-      release_year_until: film.year,
-      // providers: ['hbm'],
-    })
-
-    if (searchResult && searchResult.items[0] && searchResult.items[0].offers) {
-      let offers = searchResult.items[0].offers
-
-      offers = offers.map((offer) => {
-        offer.provider = providers.find((provider) => provider.id === offer.provider_id)
-        return offer
-      })
-
-      // Sort by price
-      offers = offers.sort((a, b) => {
-        const priceA = a.retail_price || 0
-        const priceB = b.retail_price || 0
-        return priceA > priceB ? 1 : -1
-      })
-
-      let finalOffers = []
-      if (verbose) {
-        finalOffers = offers
-      } else {
-        // Remove duplicates
-        let uniqueOffers = []
-        offers = offers.forEach((offer) => {
-          const include = !uniqueOffers.some((unique) => {
-            return offer.provider_id === unique.provider_id
-          })
-          if (include) {
-            uniqueOffers.push(offer)
-          }
-        })
-
-        // Limit results
-        finalOffers = uniqueOffers.slice(0, 4)
-      }
-
-      console.log('Available at:')
-      finalOffers.forEach((offer) => {
-        if (offer.provider) {
-          let price = ''
-          if (offer.retail_price) {
-            price = ` - $${offer.retail_price}`
-          }
-          let format = ''
-          if (verbose) {
-            format = ` - ${offer.presentation_type}`
-          }
-          console.log(`- ${offer.provider.clear_name}${format}${price}`)
-        }
-      })
-    }
-    return searchResult
+    console.log('Check availability:')
+    const query = encodeURIComponent(film.title)
+    console.log(`https://www.justwatch.com/us/search?q=${query}`)
   }
 }
 
@@ -121,9 +63,6 @@ const help = `
 
 Show film info and where to watch by rank:
 $ 1000films [number]
-
-Show more verbose results:
-$ 1000films [number] -v
 
 Search for films:
 $ 1000films [search string]
@@ -134,7 +73,6 @@ $ 1000films -l
 
 const args = process.argv.slice(2)
 const query = args[0]
-const verbose = args.includes('-v')
 if (query) {
   if (query === '-l') {
     listAll()
--- readme.md
+++ readme.md
@@ -2,32 +2,26 @@
 
 # 1000films
 
-a command line program to find where to watch the 1000 greatest films as ranked by theyshootpictures.com
+a command line program to search the 1000 greatest films as ranked by theyshootpictures.com, 2020 edition
 
 ## How to use
 
-### Install:
+### Install
 
 `npm i -g 1000films`
 
-### Show film info and where to watch by rank:
+### Show film info and where to watch by rank
 
 `1000films [number]`
 
-### Show more verbose where to watch results:
-
-`1000films [number] -v`
-
-### Search for films:
+### Search for films
 
 `1000films [search string]`
 
-### List all films:
+### List all films
 
 `1000films -l`
 
 ## Credits
 
 I have converted the ranking data into a json format for easier parsing but the film ranking data is, I presume, copyright 2020 theyshootpictures.com.
-
-Thanks to justwatch.com for streaming information and, of course, theyshootpictures.com!
